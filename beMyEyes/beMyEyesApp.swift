2fcd69933736d61fde6875bcc62afb46c22178cf
Salah-eddine Chnitifa <salah.chnitfa@gmail.com>
2022-05-28 10:56:10 +0100

    Initial Commit

---
+++ beMyEyes/beMyEyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  beMyEyesApp.swift
+//  beMyEyes
+//
+//  Created by SALAHEDDINE on 28/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct beMyEyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
